d9f3842b61a58a20fdabcb72fd25f2fdeece4907
Alisson Deives <alissondeives70@gmail.com>
2022-05-02 16:31:21 -0300

    Enviando email via Javamail

--- enviando-email/src/test/java/enviando/email/AppTest.java
+++ enviando-email/src/test/java/enviando/email/AppTest.java
@@ -1,16 +1,20 @@
 package enviando.email;
 
 import java.util.Properties;
-
+import javax.mail.Address;
 import javax.mail.Authenticator;
+import javax.mail.Message;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class AppTest {
 
 	private String userName = "teste.javaprojects@gmail.com";
 	
-	private String password = "*********";
+	private String password = "********";
 	
 	@org.junit.Test
 	public void  testeEmail() { 
@@ -18,7 +22,7 @@ public class AppTest {
 		try {
 			Properties properties = new Properties();
 			properties.put("mail.smtp.auth", "true"); //Autorização
-			properties.put("mail.smtp.starttls", "true"); //Autenticação
+			properties.put("mail.smtp.starttls.enable", "true"); //Autenticação
 			properties.put("mail.smtp.host", "smtp.gmail.com"); //Servidor do gmail
 			properties.put("mail.smtp.port", "465"); //Porta do servidor
 			properties.put("mail.smtp.socketFactory.port", "465"); //Especifica a porta a ser conectada pelo socket
@@ -31,7 +35,15 @@ public class AppTest {
 				}
 			});
 			
-			System.out.println(password);
+			Address [] toUser = InternetAddress.parse("alissondeives70@gmail.com, fafa.restier@gmail.com");
+			
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(userName)); //Quem esta enviando
+			message.setRecipients(Message.RecipientType.TO, toUser); // Quem esta recebendo
+			message.setSubject("E-mail enviado pelo código Java"); // Assunto do email
+			message.setText("Esta mensagem foi enviada pelo código Java, através do JavaMail"); //Corpo do email
+			
+			Transport.send(message);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
